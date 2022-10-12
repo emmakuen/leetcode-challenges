fea96dacaf4c61c488dd15931fa865eca21488e3
Emma <emmaku@protonmail.com>
2022-10-12 11:21:07 +0900

    add solution of number-of-one-bits

---
+++ number-of-one-bits.js
@@ -0,0 +1,45 @@
+// Write a function that takes an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+// Note:
+
+// Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type.
+// It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+// In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+
+// Example
+// Input: n = 00000000000000000000000000001011
+// Output: 3
+// Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits.
+// Input: n = 00000000000000000000000010000000
+// Output: 1
+// Explanation: The input binary string 00000000000000000000000010000000 has a total of one '1' bit.
+// Input: n = 11111111111111111111111111111101
+// Output: 31
+// Explanation: The input binary string 11111111111111111111111111111101 has a total of thirty one '1' bits.
+
+// Constraints
+// The input must be a binary string of length 32.
+
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+var hammingWeight = function (n) {
+  let count = 0;
+  while (n) {
+    count += n % 2;
+    n = Math.floor(n / 2);
+  }
+
+  return count;
+};
+
+var hammingWeight2 = function (n) {
+  let count = 0;
+  while (n) {
+    n &= n - 1;
+    count++;
+  }
+
+  return count;
+};
