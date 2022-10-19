3d13c61847488ab543581150beb03a24374b5625
Emma <emmaku@protonmail.com>
2022-10-19 22:38:16 +0900

    add comments to word-search-ii

--- word-search-ii.js
+++ word-search-ii.js
@@ -56,24 +56,39 @@ class WordDictionary {
    * @return {boolean}
    */
   search(word) {
+    // the dot can be any character, so we have to recursively search through the word dictionary for a path that has the remaining characters in the search word
+    // therefore, we should implement the depth-first search
+
     const dfs = (root, index) => {
       let current = root;
 
+      // iterate over each character of the word starting from the given index
       for (let i = index; i < word.length; i++) {
         const char = word[i];
+        // if the current character is a dot,
         if (char === ".") {
           for (const child of Object.values(current.children)) {
+            // call dfs recursively on each child of the current node
+            // return true if dfs finds the word
             if (dfs(child, i + 1)) return true;
           }
 
+          // if there's no match after dfs, return false
           return false;
+
+          // if the current character is not a dot and it's not in the current node's children, there's no match
+          // so return false
         } else if (!(char in current.children)) return false;
+        // if the current character is not a dot and it's in the current node's children, move the current pointer to that child node
         else current = current.children[char];
       }
 
+      // if the word is found, return true if it's the end of the word
+      // return false if it's not the end of the word
       return current.isEndOfWord;
     };
 
+    // call the dfs function and return its result
     return dfs(this.root, 0);
   }
 }
