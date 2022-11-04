1a5be57db384da913fbef3bbe3d0dfb4779e81df
Emma <emmaku@protonmail.com>
2022-11-04 13:38:29 +0900

    add comments to word-search solution

--- word-search.js
+++ word-search.js
@@ -36,11 +36,13 @@ const exist = function (board, word) {
   let isFound = false;
 
   function dfs(row, column, wordIndex) {
+    // if all the characters are found, set the isFound variable to true and exit the function
     if (wordIndex >= word.length) {
       isFound = true;
       return;
     }
 
+    // if row or column is out of bound or current character (board[row][column]) is not the character we're looking for (word[wordIndex]) or the word is already found, exit the function
     if (
       row >= ROW_LENGTH ||
       row < 0 ||
@@ -51,20 +53,29 @@ const exist = function (board, word) {
     )
       return;
 
+    // store the current cell character in a constant and update the cell's value to empty string to avoid reusing it in the recursive calls
     const character = board[row][column];
     board[row][column] = "";
 
+    // recursively call the function on the vertically and horizontally adjacent cells:
+    // cell below
     dfs(row + 1, column, wordIndex + 1);
+    // cell above
     dfs(row - 1, column, wordIndex + 1);
+    // cell on the right
     dfs(row, column + 1, wordIndex + 1);
+    // cell on the left
     dfs(row, column - 1, wordIndex + 1);
 
+    // restore the original value of the cell after the recursive calls
     board[row][column] = character;
   }
 
   for (let row = 0; row < ROW_LENGTH; row++) {
     for (let column = 0; column < COLUMN_LENGTH; column++) {
       if (isFound) return true;
+
+      // if a cell's value is equal to the starting character of the word, start the search
       if (board[row][column] === word[0]) {
         dfs(row, column, 0);
       }
