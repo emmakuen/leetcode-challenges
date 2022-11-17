9d2bef628320b0578bc54e621eb024f1be5bc0a9
Emma <emmaku@protonmail.com>
2022-11-17 13:58:29 +0900

    update the solution of longest-palindromic-substring

--- longest-palindromic-substring.js
+++ longest-palindromic-substring.js
@@ -11,35 +11,31 @@
 // s consist of only digits and English letters.
 
 function longestPalindrome(s) {
-  if (typeof s !== "string") return "";
-  if (s.length < 1 || s.length > 1000) return "";
-  if (s.length === 1) return s;
+  if (s.length < 2) return s;
 
   let maxLength = 1;
   let startIndex = 0;
 
-  const expandAroundMiddle = (left, right) => {
-    // while letters at index left && right mirror each other
-    while (left >= 0 && right < s.length && s[left] === s[right]) {
-      // if substring is longer than maxLength, make it the new maxLength
-      const substringLength = right - left + 1;
-      if (substringLength > maxLength) {
-        maxLength = substringLength;
-        startIndex = left;
-      }
-      // expand the substring to left and right
-      left--;
-      right++;
-    }
-  };
+  function expand(leftIndex, rightIndex) {
+    if (
+      leftIndex < 0 ||
+      rightIndex >= s.length ||
+      s[leftIndex] !== s[rightIndex]
+    )
+      return;
 
-  for (let i = 0; i < s.length; i++) {
-    // expand for string of odd length
-    expandAroundMiddle(i - 1, i + 1);
-    // expand for string of even length
-    expandAroundMiddle(i, i + 1);
+    const substringLength = rightIndex - leftIndex + 1;
+    if (maxLength < substringLength) {
+      maxLength = substringLength;
+      startIndex = leftIndex;
+    }
+    expand(leftIndex - 1, rightIndex + 1);
   }
 
-  // return the substring
+  for (let i = 0; i < s.length; i++) {
+    expand(i - 1, i + 1);
+    expand(i, i + 1);
+  }
+
   return s.slice(startIndex, startIndex + maxLength);
 }
