8c6b09f26e407dc5a5af128c5587ad9c5b13fcac
Emma <emmaku@protonmail.com>
2022-12-09 11:19:48 +0900

    update solution of valid-palindrome

--- valid-palindrome.js
+++ valid-palindrome.js
@@ -6,30 +6,44 @@
 // Alphanumeric characters include letters and numbers.
 // Given a string s, return true if it is a palindrome, or false otherwise.
 
-function isPalindrome(s) {
-  if (typeof s !== "string") return false;
-  if (s.length <= 1) return true;
-
-  // Sanitize input to convert it lowercase and remove non-alphanumerics
-  s = s.toLocaleLowerCase().replace(/[\W_]/g, "");
-
-  // Initialize pointers
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
   let left = 0;
   let right = s.length - 1;
 
   while (left < right) {
-    // if the left does not mirror the right, return false
-    if (s[left] !== s[right]) return false;
+    // if the character at the left pointer is non-alphanum, move up the pointer
+    if (!isAlphaNum(s, left)) {
+      left++;
+      continue;
+    }
+
+    // else if, the character at the right pointer is non-alphanum, move down the pointer
+    if (!isAlphaNum(s, right)) {
+      right--;
+      continue;
+    }
+
+    // else if, left and right characters don't match when converted to lowercase, return false
+    if (s[left].toLowerCase() !== s[right].toLowerCase()) return false;
+
+    // if characters match, move up the left pointer and move down the right pointer for the next comparison
     left++;
     right--;
   }
 
-  // if all letters mirror each other, return true
+  // if the loop finishes without returning false, the string is palindrome
   return true;
-}
+};
 
-//// ============= NAIVE APPROACH =============== //
-//// split to create an array of letters, reverse that array and join it into a string
-//// compare it to the initial string and return true if they're equal
-//      const reverseString = s.split("").reverse().join("");
-//      return s === reverseString;
+function isAlphaNum(str, index) {
+  const charcode = str.charCodeAt(index);
+  const isNumber = charcode >= 48 && charcode <= 57;
+  const isUpperLetter = charcode >= 65 && charcode <= 90;
+  const isLowerLetter = charcode >= 97 && charcode <= 122;
+
+  return isUpperLetter || isLowerLetter || isNumber;
+}
