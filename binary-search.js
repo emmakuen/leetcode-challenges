6c6edb27b5fa9fb21e7ae291d259bffe41b73ff5
Emma <emmaku@protonmail.com>
2022-12-12 16:44:00 +0900

    add solution to binary search

---
+++ binary-search.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const search = function (nums, target) {
+  let minIndex = 0;
+  let maxIndex = nums.length - 1;
+
+  while (minIndex <= maxIndex) {
+    let midIndex = Math.floor((minIndex + maxIndex) / 2);
+
+    if (nums[midIndex] === target) return midIndex;
+    // if the number at the middle index is greater than the target, all the numbers to the right side of it are also greater than target
+    // so, shrink the search window to exclude these numbers
+    if (nums[midIndex] > target) {
+      maxIndex = midIndex - 1;
+      // if the mid number is less than the target, all the numbers on the left side of the mid index are also less than the target
+      // so exclude them from the search window
+    } else {
+      minIndex = midIndex + 1;
+    }
+  }
+
+  // if target hasn't been found, return -1
+  return -1;
+};
+
+// console.log("result", search([5], 5));
+// console.log("result", search([-1, 0, 3, 5, 9, 12], 9));
+console.log("result", search([-1, 0, 3, 5, 9, 12], 2));
+console.log("result", search([-1, 0, 3, 5, 9, 12], 2));
