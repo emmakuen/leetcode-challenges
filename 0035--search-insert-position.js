2f9b9fa0420bb37a7fd75197e3ba7a213daa20f8
Emma <emmaku@protonmail.com>
2023-02-20 20:22:12 +0900

    update solution of problem 35

--- 0035--search-insert-position.js
+++ 0035--search-insert-position.js
@@ -1,25 +1,34 @@
+// Recursive Solution
 /**
  * @param {number[]} nums
  * @param {number} target
  * @return {number}
  */
-var searchInsert = function (
-  nums,
-  target,
-  startIndex = 0,
-  endIndex = nums.length - 1
-) {
-  if (startIndex >= endIndex) {
-    return nums[startIndex] >= target ? startIndex : startIndex + 1;
+var searchInsert = function (nums, target, start = 0, end = nums.length - 1) {
+  if (start >= end) return nums[start] >= target ? start : start + 1;
+
+  let mid = Math.floor((start + end) / 2);
+
+  if (nums[mid] > target) return searchInsert(nums, target, start, mid - 1);
+  if (nums[mid] < target) return searchInsert(nums, target, mid + 1, end);
+
+  return mid;
+};
+
+// Iterative Solution
+const searchInsert1 = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
   }
 
-  let midIndex = Math.floor((startIndex + endIndex) / 2);
-
-  if (nums[midIndex] > target) {
-    return searchInsert(nums, target, startIndex, midIndex - 1);
-  } else if (nums[midIndex] < target) {
-    return searchInsert(nums, target, midIndex + 1, endIndex);
-  } else {
-    return midIndex;
-  }
+  return nums[left] > target ? left + 1 : left;
 };
