485eba72060fd51dd1d624d47e81575f263aa6e6
Emma <emmaku@protonmail.com>
2023-02-21 19:57:55 +0900

    add solution of problem 540

---
+++ 0540--single-element-in-sorted-array.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function (nums) {
+  let start = 0;
+  let end = nums.length - 1;
+
+  while (start < end) {
+    const mid = Math.floor((start + end) / 2);
+    const isEvenlyDivided = !(mid % 2);
+    if (nums[mid] !== nums[mid + 1] && nums[mid] !== nums[mid - 1])
+      return nums[mid];
+
+    (isEvenlyDivided && nums[mid] === nums[mid - 1]) ||
+    (!isEvenlyDivided && nums[mid] === nums[mid + 1])
+      ? (end = mid - 1)
+      : (start = mid + 1);
+  }
+
+  return nums[start];
+};
