93e46d52c52293a6cc146b245f32a30ec0a3d3b7
Emma <emmaku@protonmail.com>
2023-02-21 20:07:06 +0900

    update the solution syntax

--- 0540--single-element-in-sorted-array.js
+++ 0540--single-element-in-sorted-array.js
@@ -9,8 +9,9 @@ var singleNonDuplicate = function (nums) {
   while (start < end) {
     const mid = Math.floor((start + end) / 2);
     const isEvenlyDivided = !(mid % 2);
-    if (nums[mid] !== nums[mid + 1] && nums[mid] !== nums[mid - 1])
+    if (nums[mid] !== nums[mid + 1] && nums[mid] !== nums[mid - 1]) {
       return nums[mid];
+    }
 
     (isEvenlyDivided && nums[mid] === nums[mid - 1]) ||
     (!isEvenlyDivided && nums[mid] === nums[mid + 1])
