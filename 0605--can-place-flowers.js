7ffc8289fd4955ae711e6c780dd9a0661194fb83
Emma <emmaku@protonmail.com>
2023-03-20 20:45:03 +0900

    add solution of problem 605

---
+++ 0605--can-place-flowers.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/can-place-flowers/
+
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+  let count = 0;
+
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (flowerbed[i] === 1) continue;
+
+    if (
+      (i === 0 || flowerbed[i - 1] === 0) &&
+      (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+    ) {
+      flowerbed[i] = 1;
+      count++;
+    }
+  }
+
+  return count >= n;
+};
